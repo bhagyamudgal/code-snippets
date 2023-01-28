a501b36f9381f26702b0788a9b89c220798597eb
Bhagya Mudgal <76253604+bhagyamudgal@users.noreply.github.com>
2023-01-28 16:41:59 +0530

    Added shortenWalletAddress Snippet

---
+++ web3/shortenWalletAddress.ts
@@ -0,0 +1,7 @@
+export const shortenWalletAddress = (address: string) => {
+    if (address) {
+        return address.slice(0, 4) + "..." + address.slice(-4);
+    } else {
+        return "---";
+    }
+};
