b942a9d0b5aef1280240c0c3fce25dc4e56f54b8
Bhagya Mudgal <76253604+bhagyamudgal@users.noreply.github.com>
2023-02-17 22:56:36 +0530

    add better loading spinner

--- general/components/LoadingSpinner.tsx
+++ general/components/LoadingSpinner.tsx
@@ -1,15 +1,37 @@
 import { Center, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
 
 interface Props {
     loadingText?: string;
-    color?: string;
+    size?: "xs" | "sm" | "md" | "lg" | "xl";
+    color: string;
 }
 
-function LoadingSpinner({ loadingText = "", color = "blue.500" }: Props) {
+function LoadingSpinner({
+    loadingText,
+    size = "xl",
+    color = "primary.500",
+}: Props) {
     return (
-        <Center color={color} my={20} flexDirection="column">
-            <Spinner size="lg" mb={3} />
-            {loadingText && <Text>{loadingText}</Text>}
+        <Center flexDirection="column" minH="250px">
+            <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color={color}
+                size={size}
+                mb={3}
+            />
+            {loadingText && (
+                <Text
+                    textAlign="center"
+                    maxW="280px"
+                    color={color}
+                    fontFamily="lexend"
+                >
+                    {loadingText}
+                </Text>
+            )}
         </Center>
     );
 }
